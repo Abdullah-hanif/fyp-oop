c71f8a1f067648f01e0e508d969f4867a379e5cd
Abdullah hanif <akif.abdullah.aa32@gmail.com>
2023-06-06 18:00:32 +0500

    employee management system with local db"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from './Page/Dashboard';
+
+function App() {
+  return (
+    <div className="background-gradient" style={{minHeight:'750px'}}>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/index.js
@@ -0,0 +1,21 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Susan',
+    lastName: 'Jordon',
+    email: 'susan@example.com',
+    salary: '95000',
+    date: '2019-04-11'
+  },
+  {
+    id: 2,
+    firstName: 'Akif',
+    lastName: 'khan',
+    email: 'akif@example.com',
+    salary: '80000',
+    date: '2023-04-17'
+  },
+
+];
+
+export { employeesData };
